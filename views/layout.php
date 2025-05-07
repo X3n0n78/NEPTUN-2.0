9e014672b3cfbb320cfd52c1a0941c0baa8e686e
X3n0n78 <31189580+X3n0n78@users.noreply.github.com>
2025-05-07 14:45:51 +0200

    debug

--- views/layout.php
+++ views/layout.php
@@ -1,16 +1,35 @@
-<ul>
-    <?php foreach ($config['menu'] as $key => $value): ?>
-        <li><a href="index.php?page=<?= $key ?>"><?= $value ?></a></li>
-    <?php endforeach; ?>
-    <?php if (!isset($_SESSION['user'])): ?>
-        <li><a href="index.php?page=login">Belépés</a></li>
-    <?php else: ?>
-        <li><a href="index.php?page=logout">Kilépés</a></li>
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <title><?= $title ?? 'Tanulónyilvántartó rendszer' ?></title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <nav>
+        <ul>
+            <?php foreach ($config['menu'] as $key => $value): ?>
+                <li><a href="index.php?page=<?= $key ?>"><?= $value ?></a></li>
+            <?php endforeach; ?>
+            <?php if (!isset($_SESSION['user'])): ?>
+                <li><a href="index.php?page=login">Belépés</a></li>
+            <?php else: ?>
+                <li><a href="index.php?page=logout">Kilépés</a></li>
+            <?php endif; ?>
+        </ul>
+    </nav>
+
+    <?php if (isset($_SESSION['user'])): ?>
+        <div class="logged-in">
+            Bejelentkezett:
+            <?= htmlspecialchars($_SESSION['user']['lastname']) ?>
+            <?= htmlspecialchars($_SESSION['user']['firstname']) ?>
+            (<?= htmlspecialchars($_SESSION['user']['username']) ?>)
+        </div>
     <?php endif; ?>
-</ul>
-<?php if (isset($_SESSION['user'])): ?>
-    <div>Bejelentkezett: <?= htmlspecialchars($_SESSION['user']['lastname']) ?>
-        <?= htmlspecialchars($_SESSION['user']['firstname']) ?>
-        (<?= htmlspecialchars($_SESSION['user']['username']) ?>)
-    </div>
-<?php endif; ?>
+
+    <main>
+        <?php if (isset($content)) include $content; ?>
+    </main>
+</body>
+</html>
